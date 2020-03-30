b14274c2438edc66151e8bea5469eeb16b9bd5cd
Arief Luthfi Aulia <57087359+ariefluthfi90@users.noreply.github.com>
2020-03-30 23:37:25 +0700

    [UPDATE] Add model (Quote and Value)

---
+++ src/main/java/id/arief/resttemplate/ConsumingRestApplication.java
@@ -0,0 +1,13 @@
+package id.arief.resttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumingRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumingRestApplication.class, args);
+    }
+
+}
